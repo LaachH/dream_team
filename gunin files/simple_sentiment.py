adf934cd9802321b56e159740585aff85bf2265d
Gunin Khanna <guninshahkhanna@gmail.com>
2022-04-05 19:24:04 +1000

    adding sentiment analysis models (nltk and text blob)

---
+++ gunin files/simple_sentiment.py
@@ -0,0 +1,16 @@
+
+
+
+class SimpleSentiment():
+    def __init__(self):
+        pass
+
+    def clean_text(self, input_str):
+        # normalising text
+        normalised_str = input_str.lower()
+
+        
+
+    def tb_score(self):
+        pass
+
